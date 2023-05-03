7bf720ed6957e7f5a9c1fa7efce21b1b21ab44b7
Cyberyzy <Arzfall@outlook.com>
2023-05-03 21:46:43 +0800

    Deployed 025cdfe with MkDocs version: 1.4.3

--- javascripts/extra.js
+++ javascripts/extra.js
@@ -3,3 +3,17 @@ document.querySelectorAll('.zoom').forEach(item => {
         this.classList.toggle('image-zoom-large');
     })
 });
+document.addEventListener("DOMContentLoaded", function() {
+    var nav = document.getElementsByClassName("md-nav");
+    for(var i = 0; i < nav.length; i++) {
+        if (nav.item(i).getAttribute("data-md-level")) {
+            nav.item(i).style.display = 'block';
+            nav.item(i).style.overflow = 'visible';
+        }
+    }
+
+    var nav = document.getElementsByClassName("md-nav__toggle");
+    for(var i = 0; i < nav.length; i++) {
+       nav.item(i).checked = true;
+    }
+});
